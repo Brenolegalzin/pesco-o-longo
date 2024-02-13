2de3d91b56bde489fac51c4808065385d66c22c7
Brenolegalzin <145219656+Brenolegalzin@users.noreply.github.com>
2024-02-13 00:29:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ document.getElementById("imagem").innerText = "--------\n   |   |  \n _____ \n__
 let quant = "\n|";
 let score = 0;
 document.onscroll = function(){
-	quant = quant+"\n|"
 	document.querySelector("#score").style.top = window.scrollY;
 	if(window.scrollY>=window.scrollMaxY){
 		score += 5;
+		quant = quant+"\n|"
 		document.getElementById("score").innerText = "Pontos :"+score
 		for(let i=0;i<=10; i++){
 			document.getElementById("imagem").innerText = "--------\n   |   |  \n _____ \n________  \n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|"+quant;
