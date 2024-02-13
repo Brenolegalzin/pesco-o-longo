9a849654f22f30deedb7518cf68a6196e3bf1be1
Brenolegalzin <145219656+Brenolegalzin@users.noreply.github.com>
2024-02-12 23:29:31 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById("imagem").innerText = "--------\n   |   |  \n _____ \n________  \n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|";
+let quant = "\n|";
+let score = 0;
+document.onscroll = function(){
+	quant = quant+"\n|"
+	document.querySelector("#score").style.top = window.scrollY;
+	if(window.scrollY==window.scrollMaxY){
+		score += 5;
+		document.getElementById("score").innerText = "Pontos :"+score
+		for(let i=0;i<=10; i++){
+			document.getElementById("imagem").innerText = "--------\n   |   |  \n _____ \n________  \n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|"+quant;
+		}
+	}
+}
