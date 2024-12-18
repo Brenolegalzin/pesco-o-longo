0bc47c15e7fe3b406f28816ed741312e505b89ab
Brenolegalzin <145219656+Brenolegalzin@users.noreply.github.com>
2024-12-17 23:35:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ let quant = "\n|";
 let score = 0;
 document.onscroll = function(){
 	document.querySelector("#score").style.top = window.scrollY;
-	if(window.innerHeight+window.scrollY-300>=document.documentElement.scrollHeight){
-		score += 5;
-		quant = quant+"\n|"
-		document.getElementById("score").innerText = "Pontos :"+score
-		for(let i=0;i<=50; i++){
-			document.getElementById("imagem").innerText = "--------\n   |   |  \n _____ \n________  \n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|"+quant;
-		}
+}
+document.onscrollend = function(){
+	score += 5;
+	quant = quant+"\n|"
+	document.getElementById("score").innerText = "Pontos :"+score
+	for(let i=0;i<=50; i++){
+		document.getElementById("imagem").innerText = "--------\n   |   |  \n _____ \n________  \n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|"+quant;
 	}
 }
